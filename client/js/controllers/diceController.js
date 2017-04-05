b02c16c74cfe33d62d9c105d943fe54f73549a17
Kristin Sztengel <kcsztengel@gmail.com>
2017-04-05 15:01:46 -0600

    started calcs

--- client/js/controllers/diceController.js
+++ client/js/controllers/diceController.js
@@ -1,13 +1,13 @@
 'use strict'
 app.controller('DiceController', function($scope, DiceService, $http) {
     console.log("in the diceController");
-    $scope.diceForm = {}
+
 
     $scope.submitDice = function(data){
 
       console.log("data", data);
-
       return $http.post('http://localhost:3000/dice', {data});
+      $scope.diceForm.$setPristine()
 
       }
 
--- server/routes/dice.js
+++ server/routes/dice.js
@@ -4,9 +4,13 @@ var router = express.Router();
 router.post('/', function(req, res) {
   //Logic goes here
   console.log("in the dice route", req.body.data)
+  let dice = req.body.data
 
-  // var example = res.json()
-  // console.log(example);
+  sides = parseInt(dice.sides)
+
+  var example = Math.floor(Math.random()*sides)+1
+
+  res.json(example)
 
 
 })
